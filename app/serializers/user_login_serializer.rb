6d2fce46d80b7a0524e6efffe2e915dc4d109a82
payne-chris-r <06cpayne@gmail.com>
2017-06-27 17:22:22 -0400

    Adds profile id to login serializer
    Now when a user logs in, their profile id is sent back with the json
    response. It will be used to lookup their profile.

--- app/serializers/user_login_serializer.rb
+++ app/serializers/user_login_serializer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 class UserLoginSerializer < ActiveModel::Serializer
-  attributes :id, :email, :token
+  attributes :id, :email, :token, :profile
+
+  def profile
+    object.profile.id if object.profile
+  end
 
   def token
     Rails.application.message_verifier(:signed_token).generate(object.token)
