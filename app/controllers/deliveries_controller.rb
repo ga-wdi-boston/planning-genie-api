333d53066f154f8d34671687ca732ca43a6ae751
payne-chris-r <06cpayne@gmail.com>
2017-06-27 08:33:54 -0400

    Adds line after frozen string literal - linter foo
    Adds a line after the frozen string literal line
    Things broke when I did what the linter was asking me to do:
    Linter was yelling about [:update, :create] => `%i(update)`
    So I reverted. I don't see why this is a setting. Remove?

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeliveriesController < OpenReadController
-  before_action :set_delivery, only: `%i(show update destroy)`
+  before_action :set_delivery, only: [:show, :update, :destroy]
 
   # GET /deliveries
   def index
--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class ExamplesController < OpenReadController
   before_action :set_example, only: [:update, :destroy]
 
