d17755da08efb2787ab13aa5f8543624463d3178
sdavidson140 <sdavidson140@gmail.com>
2017-07-07 22:18:49 -0400

    add Active Record validation on Delivery
    - check for unique cohort & material pairs
    - add custom error message
    - cohort name validation is case insensitive
    close #19

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,4 +1,6 @@
 class Delivery < ApplicationRecord
   belongs_to :material
   belongs_to :user
+  validates :cohort, uniqueness: { scope: :material, case_sensitive: false,
+    message: "Specified material is already being delivered for this cohort." }
 end
